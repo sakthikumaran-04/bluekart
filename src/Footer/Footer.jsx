303c73261c671bb936ea612245625668e78ae9ac
sakthikumaran-04 <sakthikumaranofficial@gmail.com>
2024-03-27 22:02:50 +0530

    Minor styles

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -5,16 +5,16 @@ function Footer() {
     <footer className="bg-slate-800 text-slate-300 font-body grid grid-cols-2 text-center p-5 gap-4">
       <div className="flex-1">
         <h3 className="font-medium text-lg">Shop</h3>
-        <p className="cursor-pointer">Men's</p>
-        <p className="cursor-pointer">Women's</p>
-        <p className="cursor-pointer">Electronics</p>
-        <p className="cursor-pointer">Jewelry</p>
+        <p className="cursor-pointer text-sm">Men's</p>
+        <p className="cursor-pointer text-sm">Women's</p>
+        <p className="cursor-pointer text-sm">Electronics</p>
+        <p className="cursor-pointer text-sm">Jewelry</p>
       </div>
       <div className="flex-1">
         <h3 className="font-medium text-lg">Help</h3>
-        <p className="cursor-pointer">Contact us</p>
-        <p className="cursor-pointer">FAQ</p>
-        <p className="cursor-pointer">Copyright</p>
+        <p className="cursor-pointer text-sm">Contact us</p>
+        <p className="cursor-pointer text-sm">FAQ</p>
+        <p className="cursor-pointer text-sm">Copyright</p>
       </div>
       <div className="flex gap-1 items-center justify-center">
         <i class="fa-brands fa-square-instagram text-3xl cursor-pointer active:text-slate-500"></i>
@@ -23,7 +23,7 @@ function Footer() {
         <i class="fa-brands fa-linkedin text-3xl cursor-pointer active:text-slate-500"></i>
         <i class="fa-brands fa-square-github text-3xl cursor-pointer active:text-slate-500"></i>
       </div>
-      <p>*Terms & conditions</p>
+      <p className="text-sm pt-2">*Terms & conditions</p>
     </footer>
   );
 }
